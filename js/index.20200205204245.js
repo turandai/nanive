701e7159daa619b84b7d9f9ac9cfa4c953a068a4
Turan <turandai@outlook.com>
2020-02-05 21:12:05 +0800

    .

---
+++ js/index.20200205204245.js
@@ -0,0 +1,30 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+function em1(){var c="obojwf`tuvejpApvumppl/dpn";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/nanive_en-1036-1.png' : 'images/nanive_en-518-1.png');
+
+$('.js-4').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-5').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-6').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-7').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-8').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-9').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+
+$('.js-12').attr('src', (dpi>1) ? 'images/down-44.png' : 'images/down-22.png');
+$('.js-13').attr('src', (dpi>1) ? 'images/down-44.png' : 'images/down-22.png');
+$('.js-14').attr('src', (dpi>1) ? 'images/down-44.png' : 'images/down-22.png');
+
+};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js-3')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-11')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-16')[0], "1.00s", "0.00s", 1, 100);
+wl.start();
+
+});
---
+++ js/index.20200205210832.js
@@ -0,0 +1,52 @@
+(function(d){var h=[];d.loadImages=function(a,e){"string"==typeof a&&(a=[a]);for(var f=a.length,g=0,b=0;b<f;b++){var c=document.createElement("img");c.onload=function(){g++;g==f&&d.isFunction(e)&&e()};c.src=a[b];h.push(c)}}})(window.jQuery);
+$.fn.hasAttr = function(name) { var attr = $(this).attr(name); return typeof attr !== typeof undefined && attr !== false; };
+
+function em1(){var c="obojwf`tuvejpApvumppl/dpn";var addr="mailto:";for(var i=0;i<c.length;i++)addr+=String.fromCharCode(c.charCodeAt(i)-1);window.location.href=addr;}
+
+$(document).ready(function() {
+r=function(){dpi=window.devicePixelRatio;$('.js').attr('src', (dpi>1) ? 'images/nanive_en-1036-1.png' : 'images/nanive_en-518-1.png');
+
+$('.js-8').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-10').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-12').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-14').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-16').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+$('.js-18').attr('src', (dpi>1) ? 'images/dot-44.png' : 'images/dot-22.png');
+
+$('.js-22').attr('src', (dpi>1) ? 'images/down-44.png' : 'images/down-22.png');
+$('.js-24').attr('src', (dpi>1) ? 'images/down-44.png' : 'images/down-22.png');
+$('.js-26').attr('src', (dpi>1) ? 'images/down-44.png' : 'images/down-22.png');
+
+};
+if(!window.HTMLPictureElement){r();}
+(function(){$('a[href^="#"]:not(.allowConsent,.noConsent,.denyConsent,.removeConsent)').each(function(){$(this).click(function(){var t=this.hash.length>1?$('[name="'+this.hash.slice(1)+'"]').offset().top:0;return $("html, body").animate({scrollTop:t},400),!1})})})();
+var wl = new woolite();
+wl.init();
+wl.addAnimation($('.js-2')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-3')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-4')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-6')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-7')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-8')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-9')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-10')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-11')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-12')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-13')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-14')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-15')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-16')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-17')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-18')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-20')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-21')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-22')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-23')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-24')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-25')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-26')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-28')[0], "1.00s", "0.00s", 1, 100);
+wl.addAnimation($('.js-29')[0], "1.00s", "0.00s", 1, 100);
+wl.start();
+
+});
